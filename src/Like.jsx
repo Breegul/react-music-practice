e6ac041d143ae42e72c696d5ad59a2e609c30f02
Martin <martin.torrado.mouzo@gmail.com>
2023-02-20 16:26:07 +0000

    add a heart to the like button

--- src/Like.jsx
+++ src/Like.jsx
@@ -1,10 +1,13 @@
 import React, {useState} from 'react'
 
+import empty from "./assets/heart-empty.svg"
+import filled from "./assets/heart-filled.svg"
+
 const Like = () => {
     const [loveAlbum, setLoveAlbum] = useState(false);
 
   return (
-    <button onClick={() => setLoveAlbum(!loveAlbum)}>{loveAlbum ? "I don't love this album" : "I love this album"}</button>
+    <button onClick={() => setLoveAlbum(!loveAlbum)}>{loveAlbum ? <img src={filled}/> : <img src={empty}/>}</button>
   )
 }
 
