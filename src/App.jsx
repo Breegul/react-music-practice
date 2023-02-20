6047f38aac189e99829f0e28003f287aad930421
Baskin-Ridley <gbaskinridley@gmail.com>
2023-02-20 15:03:29 +0000

    useful

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Artist from './Artist'
 
 const App = () => {
   return (
-    <div>App</div>
+
+    <Artist/>
+
   )
 }
 
---
+++ src/Artist.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Artist = () => {
+  return (
+    <>
+        <h1>Smash Mouth</h1>
+    </>
+    )
+}
+
+export default Artist
