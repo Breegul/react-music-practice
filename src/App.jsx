e688bc6235d95d0fa6d33492c52b3d56cc1edb3b
Baskin-Ridley <gbaskinridley@gmail.com>
2023-02-20 15:47:13 +0000

    add all star

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React, {useState} from 'react'
 import Artist from './Artist'
 import Albums from './Albums'
 import './app.css'
+import SmashForm from './SmashForm'
 
 const App = () => {
   return (
     <>
     <Artist/>
     <Albums/>
+    <SmashForm/>
     </>
   )
 }
---
+++ src/SmashForm.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const SmashForm = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(e.target.smash.value)
+        if(e.target.smash.value == "?"){
+            window.location.href = "https://www.youtube.com/watch?v=L_jWHffIx5E"
+
+        }
+    }
+  return (
+    <>
+    <h2>Did you know about Smash Mouth before Shrek?</h2>
+    <form onSubmit={handleSubmit}>
+    
+        <input type="radio" name="smash" id="yes" value="yes"/>
+        <label htmlFor="yes">Yes</label>
+    <br/>
+        <input type="radio" name="smash" id="no" value="no"/>
+        <label htmlFor="yes">No</label>
+    <br/>
+        <input type="radio" name="smash" id="?" value="?"/>
+        <label htmlFor="yes">Whats a shrek?</label>   
+    <br/>
+        <input type="submit" htmlFor="shrekSubmit"/>
+    </form>  
+    </>  
+    )
+}
+
+export default SmashForm
