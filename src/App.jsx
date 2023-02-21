bf8a9e16577426c46426556b695012b25d890b03
Martin <martin.torrado.mouzo@gmail.com>
2023-02-21 16:55:02 +0000

    wacky colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Artist from './Artist'
 import Albums from './Albums'
 import './app.css'
 import SmashForm from './SmashForm'
 
 const App = () => {
+  const body = document.querySelector("body");
+  useEffect(() => {
+    const genRanHex = size => [...Array(size)]
+      .map(() => Math.floor(Math.random() * 16)
+      .toString(16))
+      .join('');
+
+    body.style.backgroundColor = `#${genRanHex(6)}`
+
+  
+  }, [])
+  
   return (
     <>
     <Artist/>
--- src/Artist.jsx
+++ src/Artist.jsx
@@ -5,7 +5,6 @@ import filled from "./assets/heart-filled.svg"
 
 const Artist = () => {
 
-
     const [loveArtist, setLoveArtist] = useState(false) 
 
     const handleClick = () => {
@@ -17,7 +16,6 @@ const Artist = () => {
             <h1>Smash Mouth</h1>
             <img src="https://www.udiscovermusic.com/wp-content/uploads/2019/08/Smash-Mouth-2003-press-shot-01-CREDIT-Jay-Blakesberg-1000.jpg" alt="Smash Mouth"/>
             <p>Smash Mouth is an American rock band from San Jose, California, formed in 1994. The band currently consists of Steve Harwell (lead vocals), Greg Camp (guitar), Paul De Lisle (bass guitar), and Kevin Coleman (drums). The band is best known for their 1999 hit single "All Star", which was featured in the film Shrek. The band's other hits include "Walkin' on the Sun", "Then the Morning Comes", "Can't Get Enough of You Baby", and "I'm a Believer".</p>
-            {/* <button onClick={() => setLoveArtist(!loveArtist)}>{loveArtist ? <img src='empty'/> : <img src="filled"/>}</button> */}
             <Like/>
         </>
     )
